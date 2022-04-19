8fbf1ce3289d60b01f5b76396e307e3e5ed70936
AUGEY QUENTIN <quentin.augey@localhost.localdomain>
2022-04-19 09:46:04 +0200

    creation de l'architecture

---
+++ modele/bdd/bdd.php
@@ -0,0 +1,12 @@
+
+<?php
+
+            $servername = 'sqletud.u-pem.fr';
+            $username = '';
+            $password = '';
+            $dbname = '';
+            
+  
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+?>
